ad154862e9f442609f04f958fb473b38f2e1e2b0
marko <marcomow@gmail.com>
2021-08-10 15:52:03 +0300

    attempt with custom image

--- hass-addon-petoneer-smartdot/index.js
+++ hass-addon-petoneer-smartdot/index.js
@@ -1,23 +1,9 @@
 console.log('Petoneer addon ' + new Date());
 
-const noble = require('@abandonware/noble');
+const { createBluetooth } = require('node-ble');
 
 (async () => {
-    noble.on('stateChange', async (state) => {
-        if (state === 'poweredOn') {
-            await noble.startScanningAsync([0xfff0], false);
-        }
-    });
-    noble.on('discover', async (peripheral) => {
-        await noble.stopScanningAsync();
-        await peripheral.connectAsync();
-        const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([0xfff0]);
-        const batteryLevel = (await characteristics[0].readAsync())[0];
-
-        console.log(`${peripheral.address} (${peripheral.advertisement.localName}): ${batteryLevel}%`);
-
-        await peripheral.disconnectAsync();
-        process.exit(0);
-    });
-
+    const { bluetooth, destroy } = createBluetooth();
+    const adapter = await bluetooth.defaultAdapter();
+    console.log(adapter)
 })();
