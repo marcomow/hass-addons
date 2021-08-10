c577190f963d02668ead26d0470bb14d8f68ca43
marko <marcomow@gmail.com>
2021-08-10 13:23:46 +0300

    changing run.sh attempt

--- hass-addon-petoneer-smartdot/run.sh
+++ hass-addon-petoneer-smartdot/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-CONFIG_PATH=/data/options.json
-CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
+# CONFIG_PATH=/data/options.json
+# CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
 
 echo Started Addon
 node -v
