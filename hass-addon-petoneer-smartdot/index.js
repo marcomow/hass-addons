a31c2eda33a19f6e41f8012f10dfc6654396cd62
marko <marcomow@gmail.com>
2021-08-10 12:30:43 +0300

    error handling

--- hass-addon-petoneer-smartdot/index.js
+++ hass-addon-petoneer-smartdot/index.js
@@ -1,16 +1,20 @@
-console.log('Petoneer addon' + new Date());
+console.log('Petoneer addon ' + new Date());
 const { createBluetooth } = require('node-ble');
 (async () => {
-    const { bluetooth, destroy } = createBluetooth();
-    const adapter = await bluetooth.defaultAdapter();
-    const device = await adapter.waitDevice('34:14:B5:39:E9:A3');
-    await device.connect();
-    const gattServer = await device.gatt();
-    const service1 = await gattServer.getPrimaryService(0xfff0);
-    const characteristic1 = await service1.getCharacteristic(0xfff3);
-    const command = '0f0405000309';
-    const value = command.match(/.{1,2}/g).map(v => parseInt(v, 16));
-    await characteristic1.writeValue(new Uint8Array(value))
-    const buffer = await characteristic1.readValue()
-    console.log(buffer)
+    try {
+        const { bluetooth, destroy } = createBluetooth();
+        const adapter = await bluetooth.defaultAdapter();
+        const device = await adapter.waitDevice('34:14:B5:39:E9:A3');
+        await device.connect();
+        const gattServer = await device.gatt();
+        const service1 = await gattServer.getPrimaryService(0xfff0);
+        const characteristic1 = await service1.getCharacteristic(0xfff3);
+        const command = '0f0405000309';
+        const value = command.match(/.{1,2}/g).map(v => parseInt(v, 16));
+        await characteristic1.writeValue(new Uint8Array(value))
+        const buffer = await characteristic1.readValue()
+        console.log(buffer)
+    } catch (error) {
+        console.log(error);
+    }
 })();
